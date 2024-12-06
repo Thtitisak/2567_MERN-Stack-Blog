85e4214749cf294bf9ec3a38794123f6688ea1fd
Wuttha <Wuttha@webmail.npru.ac.th>
2024-12-06 16:59:59 +0700

    first commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,11 @@
-import Header from "./Header";
+import React, { Component } from 'react'
 
-
-const Header =() => {
-    return(
-        <div>
-        <div className="navbar bg-base-300 rounded-box">
-  <div className="flex-1 px-2 lg:flex-none">
-    <a className="text-lg font-bold">daisyUI</a>
-  </div>
-  <div className="flex flex-1 justify-end px-2">
-    <div className="flex items-stretch">
-      <a className="btn btn-ghost rounded-btn">Button</a>
-      <div className="dropdown dropdown-end">
-        <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn">Dropdown</div>
-        <ul
-          tabIndex={0}
-          className="menu dropdown-content bg-base-100 rounded-box z-[1] mt-4 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li><a>Item 2</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-        </div>
+export class Header extends Component {
+  render() {
+    return (
+      <div>Header</div>
     )
+  }
 }
+
+export default Header
